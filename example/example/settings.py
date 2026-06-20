7d1b74f5ff83f5637333396e503221c338f11df9
eligrubbs <gitcontribs@eligrubbs.com>
2026-06-19 20:05:09 -0500

    javascript build frontend working with templates.

--- example/example/settings.py
+++ example/example/settings.py
@@ -125,4 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / ".prod_static"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
